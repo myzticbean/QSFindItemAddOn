5a134d34252235cd2f2216cdc889db6ab9b0ad0b
luke <itslukemango@gmail.com>
2024-06-18 11:50:04 +0100

    feat: redo playerwarp support, show nearest warp owned by shop owner | fix: optimise imports

--- src/main/java/io/myzticbean/finditemaddon/Utils/WarpUtils/PlayerWarpsUtil.java
+++ src/main/java/io/myzticbean/finditemaddon/Utils/WarpUtils/PlayerWarpsUtil.java
@@ -22,54 +22,49 @@ import com.olziedev.playerwarps.api.warp.Warp;
 import io.myzticbean.finditemaddon.Dependencies.PlayerWarpsPlugin;
 import io.myzticbean.finditemaddon.FindItemAddOn;
 import io.myzticbean.finditemaddon.Utils.CommonUtils;
-import io.myzticbean.finditemaddon.Utils.LoggerUtils;
 import org.bukkit.Location;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.UUID;
 
 /**
- * @author myzticbean
+ * @author myzticbean & lukemango
  */
 public class PlayerWarpsUtil {
 
     @Nullable
-    public Warp findNearestWarp(Location shopLocation) {
-        List<Warp> allWarps = PlayerWarpsPlugin.getAllWarps();
-        if(!allWarps.isEmpty()) {
+    public Warp findNearestWarp(Location shopLocation, UUID shopOwner) {
+        List<Warp> playersWarps = PlayerWarpsPlugin.getAllWarps().stream()
+                .filter(warp -> warp.getWarpLocation().getWorld() != null)
+                .filter(warp -> warp.getWarpLocation().getWorld().equals(shopLocation.getWorld().getName()))
+                .filter(warp -> warp.getWarpPlayer().getUUID().equals(shopOwner))
+                .toList();
+
+        if (!playersWarps.isEmpty()) {
             Map<Double, Warp> warpDistanceMap = new TreeMap<>();
-            allWarps.forEach(warp -> {
-                warpDistanceMap.put(CommonUtils.calculateDistance3D(
-                        shopLocation.getX(),
-                        shopLocation.getY(),
-                        shopLocation.getZ(),
-                        warp.getWarpLocation().getX(),
-                        warp.getWarpLocation().getY(),
-                        warp.getWarpLocation().getZ()
-                ), warp);
-            });
-            if(!FindItemAddOn.getConfigProvider().DO_NOT_TP_IF_PLAYER_WARP_LOCKED)
-                return warpDistanceMap.entrySet().iterator().next().getValue(); // don't care if warp is locked or not
-            else {
-                // need to return a warp that is not locked
-                Iterator<Map.Entry<Double, Warp>> iterator = warpDistanceMap.entrySet().iterator();
-                // now keep looping until we find a warp that is not locked
-                while(iterator.hasNext()) {
-                    Warp warp = iterator.next().getValue();
-                    LoggerUtils.logDebugInfo("warp: " + warp.getWarpName() + " " + warp.isWarpLocked());
-                    if(!warp.isWarpLocked()) {
-                        return warp;
-                    }
+            playersWarps.forEach(warp ->
+                    warpDistanceMap.put(CommonUtils.calculateDistance3D(
+                            shopLocation.getX(),
+                            shopLocation.getY(),
+                            shopLocation.getZ(),
+                            warp.getWarpLocation().getX(),
+                            warp.getWarpLocation().getY(),
+                            warp.getWarpLocation().getZ()
+                    ), warp));
+
+            for (Map.Entry<Double, Warp> doubleWarpEntry : warpDistanceMap.entrySet()) {
+                // Is the config set to not tp if player warp is locked, and if so, is the warp locked?
+                if (FindItemAddOn.getConfigProvider().DO_NOT_TP_IF_PLAYER_WARP_LOCKED
+                        && doubleWarpEntry.getValue().isWarpLocked()) {
+                    continue;
                 }
-                LoggerUtils.logError("No PlayerWarp found that is in 'unlocked' state");
-                return null;
+
+                return doubleWarpEntry.getValue();
             }
         }
-        else {
-            return null;
-        }
+        return null;
     }
 }
