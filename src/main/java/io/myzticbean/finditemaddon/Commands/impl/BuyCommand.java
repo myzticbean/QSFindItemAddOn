bb813f67665f39d14d92407c12b7660a2660c325
luke <itslukemango@gmail.com>
2024-06-25 11:58:28 +0100

    chore: add 3 char minimum search

--- src/main/java/io/myzticbean/finditemaddon/Commands/impl/BuyCommand.java
+++ src/main/java/io/myzticbean/finditemaddon/Commands/impl/BuyCommand.java
@@ -1,7 +1,6 @@
 package io.myzticbean.finditemaddon.Commands.impl;
 
 import io.myzticbean.finditemaddon.Commands.CmdExecutorHandler;
-import io.myzticbean.finditemaddon.ConfigUtil.ConfigProvider;
 import io.myzticbean.finditemaddon.FindItemAddOn;
 import io.myzticbean.finditemaddon.Utils.Colourify;
 import org.bukkit.entity.Player;
@@ -19,7 +18,8 @@ public class BuyCommand extends AbstractCommand {
 
     @Command("finditem|shopsearch|searchshop to-buy <search>")
     private void onBuy(Player player, @Argument("search") @Greedy String search) {
-        if (search.length() > 3) player.sendMessage(Colourify.colour(FindItemAddOn.getConfigProvider().QUERY_TOO_SHORT_MSG));
+        if (search.length() <= 3)
+            player.sendMessage(Colourify.colour(FindItemAddOn.getConfigProvider().QUERY_TOO_SHORT_MSG));
 
         this.cmdExecutor.handleShopSearch(true, player, search);
     }
--- src/main/java/io/myzticbean/finditemaddon/Commands/impl/SellCommand.java
+++ src/main/java/io/myzticbean/finditemaddon/Commands/impl/SellCommand.java
@@ -18,7 +18,8 @@ public class SellCommand extends AbstractCommand {
 
     @Command("finditem|shopsearch|searchshop to-sell <search>")
     private void onSell(Player player, @Argument("search") @Greedy String search) {
-        if (search.length() > 3) player.sendMessage(Colourify.colour(FindItemAddOn.getConfigProvider().QUERY_TOO_SHORT_MSG));
+        if (search.length() <= 3)
+            player.sendMessage(Colourify.colour(FindItemAddOn.getConfigProvider().QUERY_TOO_SHORT_MSG));
 
         this.cmdExecutor.handleShopSearch(false, player, search);
     }
