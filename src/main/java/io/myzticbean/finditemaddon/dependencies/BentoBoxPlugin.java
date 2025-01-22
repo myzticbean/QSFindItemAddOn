948d95e84b0623184e4a571718c90e08ab48fbb9
Rounick <86056736+myzticbean@users.noreply.github.com>
2025-01-22 21:45:30 +0530

    bentobox integration bug fixes (#65) (#66)
    * bentobox integration bug fixes (#65)
    * version bump for simpAPI

--- src/main/java/io/myzticbean/finditemaddon/dependencies/BentoBoxPlugin.java
+++ src/main/java/io/myzticbean/finditemaddon/dependencies/BentoBoxPlugin.java
@@ -2,9 +2,11 @@ package io.myzticbean.finditemaddon.dependencies;
 
 import org.bukkit.Location;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import world.bentobox.bentobox.BentoBox;
+import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.lists.Flags;
 
 public class BentoBoxPlugin {
@@ -20,14 +22,15 @@ public class BentoBoxPlugin {
         isBentoBoxEnabled = plugin != null && plugin.isEnabled();
     }
 
-    public boolean isIslandLocked(Location loc) {
+    public boolean isIslandLocked(Location loc, Player searchingPlayer) {
         if (!isBentoBoxEnabled) {
             return false;
         }
+        User bentoboxUser = User.getInstance(searchingPlayer);
         return BentoBox.getInstance()
                 .getIslands()
                 .getIslandAt(loc)
-                .filter(island -> !island.isAllowed(Flags.LOCK))
+                .filter(island -> !island.isAllowed(bentoboxUser, Flags.LOCK))
                 .isPresent();
     }
 }
